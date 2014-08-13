b99b962c55f5fc0573632811dbe572183f225313
Kimmo Parviainen-Jalanko <kimvais@ssh.com>
2014-08-13 12:05:18 +0300

    Broke Set 1, challenge 6

--- set1.py
+++ set1.py
@@ -3,9 +3,10 @@ import string
 import unittest
 import base64
 import logging
+import itertools
 
 import binascii
-import itertools
+import math
 
 
 logger = logging.getLogger(__name__)
@@ -26,20 +27,55 @@ def xorwith_char(data, char):
     return output
 
 
-def english_freq(input):
-    FREQ = [ord(x) for x in 'ETAOIN SHRDLU']
-    MSG_CHARS = string.ascii_uppercase + string.digits + ".,- '\n"
-    data = binascii.unhexlify(input)
-    freqs = Counter(data)
-    most_common = (x[0] for x in freqs.most_common(13))
-    translation_table = itertools.product(FREQ, most_common)
-    res = list()
-    for x, y in translation_table:
-        char = x ^ y
+def english_freq(input, min_score=3):
+    FREQUENCIES = {'e': 12.02,
+                   't': 9.10,
+                   'a': 8.12,
+                   'o': 7.68,
+                   'i': 7.31,
+                   'n': 6.95,
+                   ' ': 6.10,
+                   's': 6.28,
+                   'r': 6.02,
+                   'h': 5.92,
+                   'd': 4.32,
+                   'l': 3.98,
+                   'u': 2.88,
+                   'c': 2.71,
+                   'm': 2.61,
+                   'f': 2.30,
+                   'y': 2.11,
+                   'w': 2.09,
+                   'g': 2.03,
+                   'p': 1.82,
+                   'b': 1.49,
+                   'v': 1.11,
+                   'k': 0.69,
+                   'x': 0.17,
+                   'q': 0.11,
+                   'j': 0.10,
+                   'z': 0.07}
+    if not isinstance(input, (bytes, bytearray)):
+        data = binascii.unhexlify(input)
+    else:
+        data = input
+    candidate = None
+    best = min_score
+    for char in range(256):
         output = xorwith_char(data, char)
-        if all(chr(c) in MSG_CHARS for c in output.upper()):
-            res.append(output)
-    return res if res else None
+        freqs = Counter(output)
+        histogram = bytes(x[0] for x in freqs.most_common(13))
+        if not all(chr(c) in string.printable for c in histogram):
+            continue
+        score = 0
+        for k, v in FREQUENCIES.items():
+            score += freqs[ord(k)] * v
+        score = score / len(output)
+        if score > best:
+            logger.warning('Found a candidate histogram: {} with score {} - {}'.format(bytes(histogram), score, output))
+            best = score
+            candidate = (output, char)
+    return candidate
 
 
 def xor_with_key(input, key):
@@ -78,19 +114,23 @@ class TestSet1(unittest.TestCase):
                           '746865206b696420646f6e277420706c6179')
 
     def test_freq(self):
-        candidates = english_freq('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
-        self.assertEqual(len(candidates), 1)
-        logger.warn('Decrypted: {}'.format(candidates[0]))
+        input = binascii.unhexlify('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
+        candidate, key = english_freq(input)
+        self.assertIsNotNone(candidate)
+        logger.warning('Decrypted: {}'.format(candidate))
 
     def test_4(self):
         results = list()
         with open('4.txt') as f:
             for line in f:
-                candidates = english_freq(line.strip())
-                if candidates:
-                    results.extend(candidates)
-        self.assertEqual(len(results), 1)
-        logger.warn('Decrypted: {}'.format(results[0]))
+                try:
+                    candidate, key = english_freq(binascii.unhexlify(line.strip()))
+                except TypeError:
+                    continue
+                else:
+                    results = candidate
+        logger.warning('Decrypted: {}'.format(results))
+        self.assertIsNotNone(results)
 
     def test_challenge_5(self):
         input = b"""Burning 'em, if you ain't quick and nimble
@@ -98,9 +138,60 @@ I go crazy when I hear a cymbal"""
         output = binascii.unhexlify("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"
                                     "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")
         key = b'ICE'
-        self.assertEquals(output, xor_with_key(input, key))
+        self.assertEqual(output, xor_with_key(input, key))
 
     def test_hamming(self):
         a = b"this is a test"
         b = b"wokka wokka!!!"
-        self.assertEquals(hamming(a, b), 37)
+        self.assertEqual(hamming(a, b), 37)
+
+
+    def test_chunking(self):
+        self.assertEquals(chunk_into('foobarx', 3), ['foo', 'bar', 'x'])
+
+
+def chunk_into(data, size):
+    ret = list()
+    for i in range(math.ceil(len(data) / size)):
+        ret.append(data[i * size:(i + 1) * size])
+    return ret
+
+
+def main():
+    with open('6.txt') as f:
+        data = base64.b64decode(f.read())
+    keysize_candidates = list()
+    for keysize in range(1, 30):
+        chunks = chunk_into(data, keysize)
+        pairs = itertools.combinations(chunks[:4], 2)
+        score = 0
+        for a, b in pairs:
+            hscore = hamming(a, b)
+            logger.fatal(hscore)
+            score += hscore / keysize
+        score = score / 4
+        keysize_candidates.append((keysize, score))
+    keysize_probability = sorted(keysize_candidates, key=lambda x: x[1])
+    logger.info("Probable keysizes: {}".format(keysize_probability[:5]))
+    for keysize, score in keysize_probability[:3]:
+        key_ints = list()
+        logger.debug(keysize)
+        chunks = chunk_into(data, keysize)
+        transposed = zip(*chunks[:-1])
+        for x in transposed:
+            try:
+                _, key_int = english_freq(bytes(x))
+                key_ints.append(key_int)
+            except TypeError:
+                break
+
+        if len(key_ints) == keysize:
+            print('Decrypted:')
+            print(xor_with_key(data, bytes(key_ints)).decode('ascii'))
+            return
+
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    main()
