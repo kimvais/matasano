13874c8211def9ce558653c92197f9567956368a
Kimmo Parviainen-Jalanko <kimvais@ssh.com>
2014-08-13 14:49:15 +0300

    Challenge 11, encryption oracle

--- aes.py
+++ aes.py
@@ -1,6 +1,8 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
-from tools import chunk_into, xor_with_key
+from tools import chunk_into, xor_with_key, pkcs7pad, unpad
+import random
+import os
 
 
 class ECB(object):
@@ -13,6 +15,8 @@ class ECB(object):
         return plain
 
     def encrypt(self, data):
+        if not len(data) % 16 == 0:
+            data = pkcs7pad(data, 16)
         e = self.cipher.encryptor()
         return e.update(data) + e.finalize()
 
@@ -29,4 +33,27 @@ class CBC(object):
         for block in blocks:
             plain.append(xor_with_key(ECB(self.key).decrypt(block), self._state))
             self._state = block
-        return b''.join(plain)
+        return unpad(b''.join(plain))
+
+    def encrypt(self, data):
+        if not len(data) % 16 == 0:
+            data = pkcs7pad(data, 16)
+        blocks = chunk_into(data, 16)
+        ciphertext = list()
+        for block in blocks:
+            cipherblock = ECB(self.key).encrypt(xor_with_key(block, self._state))
+            ciphertext.append(cipherblock)
+            self._state = cipherblock
+        return b''.join(ciphertext)
+
+
+def encryption_oracle(input):
+    key = os.urandom(16)
+    prefixlen = random.randint(5, 10)
+    suffixlen = random.randint(5, 10)
+    plain = os.urandom(prefixlen) + input + os.urandom(suffixlen)
+    if random.getrandbits(1):
+        cipher = ECB(key)
+    else:
+        cipher = CBC(key, os.urandom(16))
+    return cipher.encrypt(plain)
